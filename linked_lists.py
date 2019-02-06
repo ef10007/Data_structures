1bbe6113b7e9dfd76fc620d9b07c24da334dc9da
Jinny <44691775+ef10007@users.noreply.github.com>
2019-02-06 11:59:59 +0900

    Update linked_lists.py

--- linked_lists.py
+++ linked_lists.py
@@ -1,4 +1,3 @@
-
 class Node:
   def __init__(self, value, next_node=None):
     self.value = value
@@ -15,7 +14,7 @@ class Node:
 my_node = Node(44)
 print(my_node.get_value()) # 44
 
-    
+
 class LinkedList:
   def __init__(self, value=None):
     self.head_node = Node(value)
@@ -38,12 +37,30 @@ class LinkedList:
       current_node = current_node.get_next_node()
     return string_list
   
+  
+   def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    
+    # The list's second node is the current head_node's next_node
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()
+     
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node  
+  
+  
 ll = LinkedList(1)
 ll.insert_beginning(2)
 ll.insert_beginning(3)
 ll.insert_beginning(4)
 print(ll.stringify_list()) # 4 3 2 1
-
 
 
 
