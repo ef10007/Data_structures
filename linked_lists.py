14c3faeb7b3797768a8a8e2e8d471d196e421634
Jinny <44691775+ef10007@users.noreply.github.com>
2019-02-06 11:52:30 +0900

    Update linked_lists.py

--- linked_lists.py
+++ linked_lists.py
@@ -13,4 +13,38 @@ class Node:
     self.next_node = next_node
     
 my_node = Node(44)
-print(my_node.get_value())
+print(my_node.get_value()) # 44
+
+    
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+  
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+    
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + ' '
+      current_node = current_node.get_next_node()
+    return string_list
+  
+ll = LinkedList(1)
+ll.insert_beginning(2)
+ll.insert_beginning(3)
+ll.insert_beginning(4)
+print(ll.stringify_list()) # 4 3 2 1
+
+
+
+
+    
