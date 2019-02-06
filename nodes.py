0a1b9151b1c9278aa30709861278bb4bf808045d
Jinny <44691775+ef10007@users.noreply.github.com>
2019-02-06 11:44:07 +0900

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,4 +1,4 @@
-# Create the Node class below:
+
 class Node:
   def __init__(self, value, link_node=None):
     self.value = value
@@ -9,3 +9,22 @@ class Node:
   
   def get_link_node(self):
     return self.link_node
+  
+  def set_link_node(self, link_node):
+    self.link_node = link_node
+    
+blueberry = Node("blue")
+strawberry = Node("red")
+raspberry = Node("also red")
+
+raspberry.set_link_node(strawberry)
+blueberry.set_link_node(raspberry)
+
+rasps_data = blueberry.get_link_node().get_value()
+straws_data = raspberry.get_link_node().get_value()
+
+blues_data = blueberry.get_value()
+
+print(rasps_data) # also red
+print(straws_data) # red
+print(blues_data) # blue
