84157190e669760923bcddc686138f3c29cdf6b7
Jinny <44691775+ef10007@users.noreply.github.com>
2019-02-18 15:15:50 +0900

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 class Vertex: # stores some data
   def __init__(self, value):
     self.value = value
@@ -6,8 +8,8 @@ class Vertex: # stores some data
   def add_edge(self, vertex, weight = 0): # storing the edges to connected vertices and their weight
     self.edges[vertex] = weight 
 
-  def get_edges(self): # adding a new edges to its collection
-    return self.edges.keys()
+  def get_edges(self): # adding a new edges to its collection(list)
+    return list(self.edges.keys())
   
   
 class Graph: # stores all the vertices
@@ -36,3 +38,59 @@ class Graph: # stores all the vertices
         vertices_to_visit = set(self.graph_dict[current_vertex].edges.keys())
         start += [vertex for vertex in vertices_to_visit if vertex not in seen]
     return False
+
+  
+def print_graph(graph):
+  for vertex in graph.graph_dict:
+    print("")
+    print(vertex + " connected to")
+    vertex_neighbors = graph.graph_dict[vertex].edges
+    if len(vertex_neighbors) == 0:
+      print("No edges!")
+    for adjacent_vertex in vertex_neighbors:
+      print("=> " + adjacent_vertex)
+
+
+def build_graph(directed):
+  g = Graph(directed)
+  vertices = []
+  for val in ['a', 'b', 'c', 'd', 'e', 'f', 'g']:
+    vertex = Vertex(val)
+    vertices.append(vertex)
+    g.add_vertex(vertex)
+
+  for v in range(len(vertices)):
+    v_idx = randrange(0, len(vertices) - 1)
+    v1 = vertices[v_idx]
+    v_idx = randrange(0, len(vertices) - 1)
+    v2 = vertices[v_idx]
+    g.add_edge(v1, v2, randrange(1, 10))
+
+  print_graph(g)
+
+build_graph(False)
+###
+a connected to
+=> f
+
+b connected to
+=> d
+
+g connected to
+No edges!
+
+e connected to
+=> c
+
+c connected to
+=> e
+=> d
+
+f connected to
+=> a
+
+d connected to
+=> b
+=> c
+=> d
+
